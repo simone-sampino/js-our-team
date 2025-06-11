267642d44e7118a07f5853447cc80168032e2020
simone-sampino <s.samp98@gmail.com>
2025-06-11 17:47:06 +0200

    Implement team member rendering and markup generation in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,4 +39,38 @@ const teamMembers = [
 
 // DOM
 const rowEl = document.querySelector(".row");
-console.log(rowEl);
+// console.log(rowEl);
+
+renderTeamMembers(teamMembers, rowEl);
+
+function renderTeamMembers(teamMembers, nodeEl) {
+  for (let i = 0; i < teamMembers.length; i++) {
+    const member = teamMembers[i];
+    // console.log(member);
+
+    const memberMarkup = generateMembersMarkup(member);
+    // console.log(memberMarkup);
+
+    nodeEl.insertAdjacentHTML("beforeend", memberMarkup);
+  }
+}
+
+function generateMembersMarkup(member) {
+  const { name, role, email, img } = member;
+  // console.log(name, role, email, img);
+
+  const memberMarkup = `
+    <div class="col">
+      <div class="card">
+          <div class="card-body">
+              <img src="assets/${img}" alt="" class="card-img-top">
+              <h3>${name}</h3>
+              <div>${role}</div>
+              <div>${email}</div>
+          </div>
+      </div>
+    </div>
+    `;
+
+  return memberMarkup;
+}
